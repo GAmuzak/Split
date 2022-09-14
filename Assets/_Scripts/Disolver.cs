3767f2db27df4483d0d08320d8ea1565fab1060a
Rishav Banerjee <thatamuzak@gmail.com>
2022-09-13 23:44:18 -0600

    Error resolved

--- Assets/_Scripts/Disolver.cs
+++ Assets/_Scripts/Disolver.cs
@@ -5,33 +5,40 @@ using UnityEngine;
 
 public class Disolver : MonoBehaviour
 {
-    private Renderer cloneRenderer;
+    [SerializeField]private Renderer cloneRenderer;
+    [Range(0,1)][SerializeField] private float currentVal;
+    
+    
     private bool isAnimOngoing;
-    private float targetVal, currentVal, time;
+    private float targetVal, time;
+    private float currentTime;
     private static readonly int ScaleValue = Shader.PropertyToID("_scaleValue");
     private static readonly int Enabled = Shader.PropertyToID("_enabled");
 
     private void Start()
     {
-        cloneRenderer = transform.GetChild(0).GetComponent<Renderer>();
+        // cloneRenderer = transform.GetChild(0).GetComponent<Renderer>();
     }
 
     private void Update()
     {
-        if (!isAnimOngoing) return;
-        currentVal = Mathf.Lerp(currentVal, targetVal, time);
+        // if (!isAnimOngoing) return;
+        // currentTime += Time.deltaTime;
+        // currentVal = Mathf.Lerp(currentVal, targetVal, currentTime/time);
         cloneRenderer.material.SetFloat(ScaleValue, currentVal);
-        if (!Mathf.Approximately(currentVal, targetVal)) return;
-        isAnimOngoing = false;
-        cloneRenderer.material.SetInt(Enabled, 0);
+        // Debug.Log(currentVal);
+        // if (!Mathf.Approximately(currentVal, targetVal)) return;
+        // cloneRenderer.material.SetInt(Enabled, 1);
+        // currentTime = 0;
+        // isAnimOngoing = false;
     }
 
     public void Dissolve(float _targetVal, float _time)
     {
         currentVal = 1 - _targetVal;
         targetVal = _targetVal;
-        time = _time;
-        // cloneRenderer.material.SetFloat(Enabled, 1);
+        time = 100*_time;
+        cloneRenderer.material.SetInt(Enabled, 0);
         isAnimOngoing = true;
     }
 }
